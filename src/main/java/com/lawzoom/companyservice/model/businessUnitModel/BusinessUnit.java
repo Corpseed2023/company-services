0eec834e4665cee9edc83cd0b118eb12e9dec2b2
corpseed <132264141+Corpseed2023@users.noreply.github.com>
2023-11-18 14:43:43 +0530

    net work IP

--- src/main/java/com/lawzoom/companyservice/model/businessUnitModel/BusinessUnit.java
+++ src/main/java/com/lawzoom/companyservice/model/businessUnitModel/BusinessUnit.java
@@ -1,79 +1,79 @@
-package com.lawzoom.companyservice.model.businessUnitModel;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.lawzoom.companyservice.model.companyModel.Company;
-import com.lawzoom.companyservice.model.gstModel.Gst;
-
-//import javax.persistence.*;
-//import javax.validation.constraints.NotNull;
-
-
-import jakarta.persistence.*;
-
-import jakarta.validation.constraints.NotNull;
-import lombok.*;
-import org.hibernate.annotations.Comment;
-
-
-import java.util.Date;
-import java.util.List;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@Builder
-@Entity
-@Table(name = "business_unit")
-public class BusinessUnit {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
-	private Long id;
-
-//	@ManyToOne(targetEntity = Gst.class,fetch = FetchType.LAZY)
-//	@JoinColumn(name = "gst_id",nullable = false)
-//	private Gst gst;
-
-	@JsonIgnore
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "company_id")
-	private Company company;
-	
-	@NotNull
-	@Column(name = "business_activity")
-	private String businessActivity;
-	
-	@NotNull
-	private String city;
-	
-	@Column(name = "located_at")
-	private String locatedAt;
-	
-	@Column(name = "permanent_employee")
-	private int permanentEmployee;
-
-	@Column(name = "contract_employee")
-	private int contractEmployee;
-	
-	@NotNull
-	@Column(name = "address",columnDefinition = "TINYTEXT")
-	private String address;
-
-	@Column(name = "created_at")
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date createdAt;
-
-	@Column(name = "updated_at")
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date updatedAt;
-
-	@Column(length = 1,name="is_enable",columnDefinition = "tinyint(1) default 1")
-	@Comment(value = "1 : Active, 0 : Inactive")
-	private boolean isEnable;
-
-	@OneToMany(mappedBy = "businessUnit", cascade = CascadeType.ALL)
-	private List<Gst> gstList ;
-
-}
+//package com.lawzoom.companyservice.model.businessUnitModel;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//import com.lawzoom.companyservice.model.companyModel.Company;
+//import com.lawzoom.companyservice.model.gstModel.Gst;
+//
+////import javax.persistence.*;
+////import javax.validation.constraints.NotNull;
+//
+//
+//import jakarta.persistence.*;
+//
+//import jakarta.validation.constraints.NotNull;
+//import lombok.*;
+//import org.hibernate.annotations.Comment;
+//
+//
+//import java.util.Date;
+//import java.util.List;
+//
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Getter
+//@Setter
+//@Builder
+//@Entity
+//@Table(name = "business_unit")
+//public class BusinessUnit {
+//
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@Column(name = "id")
+//	private Long id;
+//
+////	@ManyToOne(targetEntity = Gst.class,fetch = FetchType.LAZY)
+////	@JoinColumn(name = "gst_id",nullable = false)
+////	private Gst gst;
+//
+//	@JsonIgnore
+//	@ManyToOne(fetch = FetchType.LAZY)
+//	@JoinColumn(name = "company_id")
+//	private Company company;
+//
+//	@NotNull
+//	@Column(name = "business_activity")
+//	private String businessActivity;
+//
+//	@NotNull
+//	private String city;
+//
+//	@Column(name = "located_at")
+//	private String locatedAt;
+//
+//	@Column(name = "permanent_employee")
+//	private int permanentEmployee;
+//
+//	@Column(name = "contract_employee")
+//	private int contractEmployee;
+//
+//	@NotNull
+//	@Column(name = "address",columnDefinition = "TINYTEXT")
+//	private String address;
+//
+//	@Column(name = "created_at")
+//	@Temporal(TemporalType.TIMESTAMP)
+//	private Date createdAt;
+//
+//	@Column(name = "updated_at")
+//	@Temporal(TemporalType.TIMESTAMP)
+//	private Date updatedAt;
+//
+//	@Column(length = 1,name="is_enable",columnDefinition = "tinyint(1) default 1")
+//	@Comment(value = "1 : Active, 0 : Inactive")
+//	private boolean isEnable;
+//
+//	@OneToMany(mappedBy = "businessUnit", cascade = CascadeType.ALL)
+//	private List<Gst> gstList ;
+//
+//}
